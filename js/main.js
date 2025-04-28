501220aad23cbce1094a8befe4810488463f5c5b
LMK <mirrol33@gmail.com>
2025-04-28 22:39:45 +0900

    04.28 update2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 // main.js
 const mainComp = {
-    template: `
+  template: `
     <div class="main-wrap">
-    
+    <section class="section01"></section>
+    <section class="section02">
+    <div class="text-box">
+    <h2>A Dream Of Adventure</h2>
+    <p>
+    A 78-year-old curmudgeonly balloon salesman, is not your average hero. When he ties thousands of balloons to his house and flies away to the wilds of South America, he finally fulfills his lifelong dream of adventure. But after Carl discovers an 8-year-old stowaway named Russell, this unlikely duo soon finds themselves on a hilarious journey in a lost wo
+    </p>
+    </div>
+    </section>
+    <section class="section03">
+        <div class="card-box">
+            <ul>
+                <li>
+                    <em>Carl Fredricksen</em>
+                </li>
+                <li>
+                    <em>Russell</em>
+                </li>
+                <li>
+                    <em>Dug</em>
+                </li>
+                <li>
+                    <em>Charles Muntz</em>
+                </li>
+            </ul>
+        </div>
+    </section>
     </div>
     `,
-}
+};
 
-export default mainComp
+export default mainComp;
